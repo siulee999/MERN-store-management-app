38e2bd346d33fdae18606953cfc7b5c60b675ab9
leah <siuleelam999@gmail.com>
2025-09-10 14:32:03 +0800

    fix bug

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.NODE_ENV === 'production' 
+const BASE_URL = import.meta.env.MODE === 'production' 
   ? '/api' 
   : 'http://localhost:8080/api';
-
+  
   
 export default axios.create({
   baseURL: BASE_URL
